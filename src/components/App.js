b41b16c972e3e3ad35480628739abfef2a74a555
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-02-23 23:16:38 +0200

    task-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
-const App = ()=>{
-    return(
-        <h1>Hello !</h1>
-    );
-}
+import Profile from './profile/Profile.js';
+import user from '../base/user.json';
+
+const App = () => {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+};
 export default App;
